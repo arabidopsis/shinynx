9ba04e467b7e8b890024cfd860685081274087aa
Ian Castleden <ian.castleden@uwa.edu.au>
2025-05-04 09:11:25 +0800

    typing

--- shinynx/sticky.py
+++ shinynx/sticky.py
@@ -2,19 +2,30 @@ from __future__ import annotations
 
 import os
 import random
+from typing import TYPE_CHECKING
 
 from shiny import App
 from starlette.middleware import Middleware
 from starlette.middleware.base import BaseHTTPMiddleware
 
 
+if TYPE_CHECKING:
+    from starlette.requests import Request
+    from starlette.responses import Response
+    from starlette.middleware.base import RequestResponseEndpoint
+
+
 class StickyCookie(BaseHTTPMiddleware):
-    def __init__(self, app: App, value: str, key: str = "sticky"):
+    def __init__(self, app: App, value: str, key: str = "sticky") -> None:
         super().__init__(app)
         self.key = key
         self.value = value
 
-    async def dispatch(self, request, call_next):
+    async def dispatch(
+        self,
+        request: Request,
+        call_next: RequestResponseEndpoint,
+    ) -> Response:
         sticky = request.cookies.get(self.key)
         response = await call_next(request)
         if not sticky:
@@ -31,7 +42,7 @@ INSTANCE_COOKIE = endpoint if endpoint else f"st-{random.random()}"
 def init_sticky(app: App) -> App:
     """Ensure app sends out a "sticky" cookie so it can be identified by nginx"""
     # see hash $cookie_sticky consistent;
-    # in sticky.conf
+    # in templates/sticky.conf
     app.starlette_app.user_middleware.append(
         Middleware(StickyCookie, value=INSTANCE_COOKIE, key="sticky"),
     )
